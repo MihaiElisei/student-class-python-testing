2c14b4bbb76d621969981e3c3508a17cfa0b28cd
Mihai Elisei <mihai.m.elisei@gmail.com>
2022-11-03 08:20:51 +0000

    add email method and test

--- test_student.py
+++ test_student.py
@@ -8,8 +8,16 @@ class TestStudent(unittest.TestCase):
         student = Student('Mihai', 'Elisei')
         self.assertEqual(student.full_name, 'Mihai Elisei')
 
+    def test_alert_santa(self):
+        student = Student('Mihai', 'Elisei')
+        student.alert_santa()
+        
+        self.assertTrue(student.naughty_list)
 
+    def test_email(self):
+        student = Student('Mihai', 'Elisei')
 
+        self.assertEqual(student.email, 'mihai.elisei@email.com')
 
 
 if __name__ == '__main__':
